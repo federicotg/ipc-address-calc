17c2b989a79b3d069c93e0f54ea2ab8924ad3aef
Federico Tello Gentile <federico.gentile@despegar.com>
2016-04-26 16:59:39 -0300

    Fix tests.

--- Calculadoras/src/test/java/org/fede/calculator/SingleMediaTest.java
+++ Calculadoras/src/test/java/org/fede/calculator/SingleMediaTest.java
@@ -71,7 +71,7 @@ public class SingleMediaTest {
     
     @Test
     public void report() throws NoSeriesDataFoundException {
-        List<InvestmentReportDTO> report = this.investments.investment("USD", false);
+        List<InvestmentReportDTO> report = this.investments.pastInvestmentsReport("USD").getDetail();
         for(InvestmentReportDTO dto : report){
             System.out.println(dto.getInflationPct());
         }
