8abf934297d37288eac909e1c01d74a320bd1dbd
Federico Tello Gentile <federico.gentile@despegar.com>
2022-05-24 13:22:20 -0300

    Fix

--- MoneyCalculator/src/main/java/org/fede/calculator/money/series/SeenPrice.java
+++ MoneyCalculator/src/main/java/org/fede/calculator/money/series/SeenPrice.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 federicogentile
+ * Copyright (C) 2022 fede
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
