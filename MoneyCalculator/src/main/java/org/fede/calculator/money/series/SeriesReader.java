f2923d4294fd4a358917ddee315e9e3672d6f878
Federico Tello Gentile <federico.gentile@despegar.com>
2022-05-24 13:20:49 -0300

    Fix

--- MoneyCalculator/src/main/java/org/fede/calculator/money/series/SeriesReader.java
+++ MoneyCalculator/src/main/java/org/fede/calculator/money/series/SeriesReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Federico Tello Gentile <federico.gentile@despegar.com>
+ * Copyright (C) 2016 Federico Tello Gentile <federicotg@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -34,7 +34,7 @@ import org.fede.calculator.money.MoneyAmount;
 
 /**
  *
- * @author Federico Tello Gentile <federico.gentile@despegar.com>
+ * @author fede
  */
 public class SeriesReader {
 
--- MoneyCalculator/src/test/java/ForeignExchangesTest.java
+++ MoneyCalculator/src/test/java/ForeignExchangesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Federico Tello Gentile <federico.gentile@despegar.com>
+ * Copyright (C) 2016 Federico Tello Gentile <federicotg@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,7 +31,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Federico Tello Gentile <federico.gentile@despegar.com>
+ * @author fede
  */
 public class ForeignExchangesTest {
 
