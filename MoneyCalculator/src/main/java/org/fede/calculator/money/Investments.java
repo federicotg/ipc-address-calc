a000cb2063bf9c1e587787afb9456aca011d9597
Federico Tello Gentile <federicotg@gmail.com>
2022-07-28 21:05:03 -0300

    Fix

--- MoneyCalculator/src/main/java/org/fede/calculator/money/Investments.java
+++ MoneyCalculator/src/main/java/org/fede/calculator/money/Investments.java
@@ -363,7 +363,7 @@ public class Investments {
             this.console.appendLine(
                     pct
                             ? this.pctBar(ym, elements)
-                            : this.bar(ym, elements, 800));
+                            : this.bar(ym, elements, 1000));
         });
 
         this.console.appendLine(format(title, nominal ? "Nominal" : "Real"));
